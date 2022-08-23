029fea8aa3e42870c78f91d8e90f3646992e12c3
jimmyheaters <jimmyheaters@users.noreply.github.com>
2022-08-23 11:35:40 -0400

    Updated keys

--- frontend/main.js
+++ frontend/main.js
@@ -2,11 +2,12 @@ window.addEventListener('DOMContentLoaded', (event) =>{
     getVisitCount();
 })
 
-const functionApi = 'http://localhost:7071/api/GetResumeCounter';
+const functionApiUrl ='https://getresumecounterbd.azurewebsites.net/api/GetResumeCounter?code=p50BzjT7DdzwfxzOSjKvp6jkLVY0Q04WBz7aFq9ynS_QAzFuEkPzCA=='
+const localfunctionApi = 'http://localhost:7071/api/GetResumeCounter';
 
 const getVisitCount = () => {
     let count = 30;
-    fetch(functionApi).then(response => {
+    fetch(functionApiUrl).then(response => {
         return response.json()
     }).then(response =>{
         console.log("Website called function API.");
